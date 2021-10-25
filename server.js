4ee631cf9cdec48a202449fc5e43e3fa7c07d48f
David F Mueth <78443941+DoctorMuch@users.noreply.github.com>
2021-10-24 21:42:10 -0500

    Add Heroku port

---
+++ server.js
@@ -0,0 +1,28 @@
+const express = require("express");
+
+const PORT = process.env.PORT || 3001;
+const app = express();
+const { notes } = require('./db/db.json');
+
+function filterByQuery(query, notesArray) {
+  let filteredResults = notesArray;
+  if(query.title) {
+    filteredResults = filteredResults.filter(note => note.title === query.title);
+  }
+  return filteredResults;
+}
+
+app.get('/api/notes', (req, res) => {
+  let results = notes;
+  if (req.query);{
+    results = filterByQuery(req.query, results);
+  }
+  res.json(results);
+});
+
+
+
+
+app.listen(3001, () => {
+  console.log(`API server now on port ${PORT}!`);
+});
