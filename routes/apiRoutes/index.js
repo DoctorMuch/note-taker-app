ac537fcc0b9e211b9374bbe1e848d281b53eda1e
David F Mueth <78443941+DoctorMuch@users.noreply.github.com>
2022-01-10 13:27:50 -0600

    branching out files to try and make this work in heroku

--- routes/apiRoutes/index.js
+++ routes/apiRoutes/index.js
@@ -1,27 +1,7 @@
 const uniqid = require('uniqid');
 const router = require('express').Router();
-const { query } = require('express');
-
+const { filterByQuery, findById, makeNote } = require('../../lib/notes');
 const { notes } = require('../../db/db.json');
-
-function filterByQuery(query, notesArray) {
-  let filteredResults = notesArray;
-  if (query.title) {
-    filteredResults = filteredResults.filter(note => note.title === query.title);
-  }
-  return filteredResults;
-}
-
-function findById(id, notesArray) {
-  const result = notesArray.filter(note => note.id === query.id)[0];
-  return result;
-};
-
-function makeNote(body, notes) {
-  const note = body;
-  notes.push(note);
-  return note;
-};
 
 router.get('/notes', (req, res) => {
   let results = notes;
