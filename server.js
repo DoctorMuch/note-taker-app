0969f6ca2b5b58c8a1b53feab8f55a4177cd1a97
David F Mueth <78443941+DoctorMuch@users.noreply.github.com>
2022-01-10 13:08:12 -0600

    working toward heroku deploy

--- server.js
+++ server.js
@@ -6,7 +6,7 @@ const app = express();
 const apiRoutes = require('./routes/apiRoutes');
 const htmlRoutes = require('./routes/htmlRoutes');
 
-app.use(express.urlencoded({ extended: true }));
+app.use(express.urlencoded({ extended: false }));
 app.use(express.json());
 app.use(express.static('public'));
 
